df5e5e585b11a7fca07524e65f8ea9df16d5a7ec
Ibrahim <Ibrahimagbor1@gmail.com>
2024-04-29 18:56:13 -0400

    need to test routes

--- backend/src/models/transaction-Model.js
+++ backend/src/models/transaction-Model.js
@@ -1,5 +1,44 @@
 const db = require('../../database/db-config');
 
 module.exports = {
-    
+    addTransaction,
+    getTransactions,
+    deleteTransactions,
+    updateTransactions
+
+}
+
+const addTransaction = async  (transaction) => {
+    const [id] = db('transactions').insert(transaction)
+    return id;
+}
+
+const getTransactions = async (user_id) => {
+    try{
+        const trans =  db('transactions').where('user_id', user_id)
+        return trans;
+    }
+    catch(err){
+        console.log('error getting your transactions', err)
+    }
+}
+
+const deleteTransactions = async (user_id) => {
+    try {
+        const deletedTrans = await db('transactions').where('user_id', user_id).del()
+        return deletedTrans
+    }
+    catch(err) {
+        console.log('error deleting your transaction', err)
+    }
+}
+
+const updateTransactions = async (user_id, updatedInfo) => {
+    try {
+        const updatedTrans = await db('transactions').where('user_id',user_id).update(updatedInfo)
+        return updatedTrans
+    } 
+    catch(err) {
+        console.log('error updating your transactions, err')
+    }
 }
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,21 +1,49 @@
 const router = require('express').Router();
 const md = require('../middlewares/authMiddleWare')
-
+const Trans = require('../models/transaction-Model')
 
 //route that adds a new transaction log
 router.post('/', md.restricted, (req,res,next) => {
-
+    const user_id = req.decoded.subject
+    Trans.addTransaction(user_id)
+        .then((response) => {
+            res.json(response)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
 })
 
 // route that retrieves a  transaction
 router.get('/', md.restricted, (req,res,next) => {
-    
+    const user_id = req.decoded.subject
+    Trans.getTransactions(user_id)
+    .then((response) => {
+        res.json(response)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
 })
 // route that updates a transaction log
 router.put('/', md.restricted, (req,res,next) => {
-    
+    const user_id = req.decoded.subject
+    Trans.updateTransactions(user_id, req.body)
+        .then((response) => {
+            res.json(response)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
 })
 // route that deletes a transaction log
 router.delete('/', md.restricted, (req,res,next) => {
-    
+    const user_id = req.decoded.subject
+    Trans.deleteTransactions(user_id)
+        .then((response) => {
+            res.json(response)
+        })
+        .catch((error) => {
+            res.json(error)
+        })
 })
